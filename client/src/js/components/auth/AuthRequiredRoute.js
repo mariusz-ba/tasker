cf60b8c3a38252b1b68e52631bcec396e4375a21
Mariusz <mariusz.baran23@gmail.com>
2017-12-08 20:26:17 +0100

    Fixed AuthRequiredRoute and added axios package to the project

--- client/src/js/components/auth/AuthRequiredRoute.js
+++ client/src/js/components/auth/AuthRequiredRoute.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 /**
  * AuthRequiredRoute component allows or not user to enter certain route.
  * Example usage:
  * <AuthRequiredRoute path="/secret" component={SecretComponent} />
  */
 
-export default class AuthRequiredRoute extends Route {
+class AuthRequiredRoute extends Route {
   render() {
     const { match } = this.state;
-    const { isAuthenticated } = this.props;
+    const isAuthenticated = this.props.session.isLoggedIn;
     if(match) {
       // Check session for authentication
       if(isAuthenticated)
@@ -23,12 +22,13 @@ export default class AuthRequiredRoute extends Route {
   }
 }
 
+
 function mapStateToProps(state) {
   return {
-    isAuthenticated: state.session.isLoggedIn
+    session: state.session
   }
 }
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps
-)(AuthRequiredRoute);
+)(AuthRequiredRoute));
