e1e5ed1e035d9f577cc9686fbf8ee2e51d1d5d0c
Mariusz <mariusz.baran23@gmail.com>
2018-01-13 14:25:41 +0100

    Separate teams router code from database queries

--- server/controllers/teamsController.js
+++ server/controllers/teamsController.js
@@ -39,9 +39,8 @@ export const getTeams = (req, res, next) => {
 export const getTeam = (req, res, next) => {
   Team.findById(req.params.id)
   .then(team => {
-    User.find({ teams: req.params.id }, { password: false }, (err, users) => {
-      res.status(200).json({ _id: team._id, name: team.name, users });
-    })
+    User.find({ teams: req.params.id }, { password: false })
+    .then(users => res.status(200).json({ _id: team._id, name: team.name, users }))
   })
   .catch(err => next(err));
 }
@@ -69,10 +68,62 @@ export const createTeam = (req, res, next) => {
   .catch(err => next(err));
 }
 
+/**
+ * Updates team with received data
+ * req.params.id - Id of a team that will be updated
+ * req.body = {
+ *    name: string - New name for that team (required)
+ *    users: [] - Array with users ids that will be assigned to that team
+ * }
+ */
 export const updateTeam = (req, res, next) => {
-
+  // Update team information
+  Team.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: {
+      name: req.body.name,
+      updatedAt: Date.now()
+    }},
+    {new: true}
+  )
+  .then(team => (
+    // Add specified users to team
+    User.update(
+      { _id: { $in: req.body.users }},
+      { $addToSet: { teams: team._id }},
+      {multi: true}
+    )
+    .then(() => (
+      // Remove other users from team
+      User.update(
+        { _id: { $nin: req.body.users }},
+        { $pull: { teams: team._id }},
+        {multi: true}
+      )
+    ))
+    .then(() => User.find({ _id: req.body.users }))
+    .then(users => res.status(200).json({
+      ...team._doc,
+      users
+    }))
+  ))
+  .catch(err => next(err));
 }
 
 export const deleteTeam = (req, res, next) => {
-
+  // Delete team with specified id
+  Team.deleteOne({ _id: req.params.id })
+  // Remove users from that team
+  .then(() => User.update(
+    { teams: req.params.id },
+    { $pull: { teams: req.params.id }},
+    { multi: true }
+  ))
+  // Remove that team from all projects that it's assigned to
+  .then(() => Project.update(
+    { teams: req.params.id },
+    { $pull: { teams: req.params.id }}
+  ))
+  .then(() => res.status(200).json(req.params.id))
+  .catch(err => next(err));
 }
--- server/routes/teams.js
+++ server/routes/teams.js
@@ -20,63 +20,24 @@ router
   else
     TeamsController.getUserTeams(req, res, next);
 })
-.get('/:id', authenticate, TeamsController.getTeam)
-.put('/', authenticate, TeamsController.createTeam)
-.post('/:id', authenticate, (req, res, next) => {
-  // Update team
-  Team.findOneAndUpdate(
-    { _id: req.params.id },
-    { $set: {
-      name: req.body.name,
-      updatedAt: Date.now()
-    }},
-    {new: true}, (err, team) => {
-      if(err) return next(err);
-      if(team) {
-        // Add users to team
-        User.update(
-          { _id: { $in: req.body.users }},
-          { $addToSet: { teams: team._id }},
-          {multi: true}, (err) => {
-            if(err) return next(err);
-            // Remove other users from team
-            User.update(
-              { _id: { $nin: req.body.users }},
-              { $pull: { teams: team._id }},
-              {multi: true}, (err, users_removed) => {
-                if(err) return next(err);
-                User.find({ _id: req.body.users }, (err, users) => {
-                  if(err) return next(err);
-                  res.status(200).json({
-                    ...team._doc,
-                    users
-                  })
-                })
-              }
-            )
-          }
-        )
-      } else {
-        res.status(404).json({ error: 'No such team' });
-      }
-    }
-  )
-})
-.delete('/:id', authenticate, (req, res, next) => {
-  // Delete team
-  console.log('Remove team: ', req.params.id);
-  Team.deleteOne({ _id: req.params.id }, (err) => {
-    if(err) return next(err);
-    // Remove users from that team
-    User.update({ teams: req.params.id }, { $pull: { teams: req.params.id }}, {multi: true}, (err) => {
-      if(err) return next(err);
-      // Remove that team from projects
-      Project.update({ teams: req.params.id }, { $pull: { teams: req.params.id }}, (err) => {
-        if(err) return next(err);
-        res.status(200).json(req.params.id);
-      })
-    })
-  })
-})
+.get  ('/:id' , authenticate, TeamsController.getTeam)
+.put  ('/'    , authenticate, TeamsController.createTeam)
+.post ('/:id' , authenticate, TeamsController.updateTeam)
+.delete('/:id', authenticate, TeamsController.deleteTeam)//(req, res, next) => {
+//   // Delete team
+//   console.log('Remove team: ', req.params.id);
+//   Team.deleteOne({ _id: req.params.id }, (err) => {
+//     if(err) return next(err);
+//     // Remove users from that team
+//     User.update({ teams: req.params.id }, { $pull: { teams: req.params.id }}, {multi: true}, (err) => {
+//       if(err) return next(err);
+//       // Remove that team from projects
+//       Project.update({ teams: req.params.id }, { $pull: { teams: req.params.id }}, (err) => {
+//         if(err) return next(err);
+//         res.status(200).json(req.params.id);
+//       })
+//     })
+//   })
+// })
 
 export default router;
